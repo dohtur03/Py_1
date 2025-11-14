374196b116d8fc7719195dc82d5894bb3f22b3da
dohtur03 <dohtur03@yandex.ru>
2025-11-14 10:26:56 +0000

    Ex. 2 completed

--- task2.py
+++ task2.py
@@ -1,26 +1,26 @@
-import sys
+# Determine if a number is a palindrome 
 
-digits = []
-palindrom = True
-num = int(sys.stdin.readline())
-sys.stdout.write(f"{str(num)}\n")
+def main():
+    num = int(input())  
+    result = process_num(num)
+    print(result)
 
-while int(num) > 0:
-    digit = (num % 10)
-    print(digit)
-    digits.append(digit)
-    num = int(num / 10)
+def process_num(num):
+    digits = []
+    palindrom = True
+    temp = num
 
-print(digits)
-half = int(len(digits)/2)
-for i in range(half):
-    start = digits[i]
-    end = digits[-(i + 1)]
-    print(start, end)
-    if start != end:
-        palindrom = False
-        print("OHOH, not PALINDROME!")
-        break
-print("OH, YES, PALINDROME!")
+    while temp > 0:
+        digit = (temp % 10)
+        digits.append(digit)
+        temp = temp // 10
 
+    half = len(digits) // 2
+    for i in range(half):
+        if digits[i] != digits[-(i + 1)]:
+            palindrom = False
+            break
+    return palindrom
 
+if __name__ == "__main__":
+    main()
