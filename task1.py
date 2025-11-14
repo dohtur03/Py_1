3f899b7336431b88acea06a942f430d79e2e6af9
dohtur03 <dohtur03@yandex.ru>
2025-11-14 10:01:56 +0000

    Ex. 1 completed

--- task1.py
+++ task1.py
@@ -1,30 +1,45 @@
-import sys
+# Calculates the scalar product of two vectors in three dimensional space
 
-all_lines = []
-all_coords = []
-counter = 0
 vectors = 2
-result = 0
 
-for coords in sys.stdin:
-    all_lines.append(coords)
-    counter += 1
-    if counter == vectors:
-        break
+def main():
+    all_lines = input_txt(vectors)
+    all_coords = process_txt(all_lines)
+    result = calc_vectors(all_coords)
+    print(result)
 
-for line in all_lines:
-    vector = []
-    numbers = line.split()
-    for number in numbers:
-        n = float(number)
-        vector.append(n)
-    all_coords.append(vector)
+# Input coordinates (3 points of 2 vectors)
+def input_txt(vectors):
+    all_lines = []
+    counter = 0
+    while counter < vectors:
+        coords = input()
+        all_lines.append(coords)
+        counter += 1
+    return all_lines
 
-lines = len(all_coords) - 1
-points = len(all_coords[0])
-for i in range(lines):
-    for j in range(points):
-        the_num = all_coords[i][j] * all_coords[i+1][j]
-        sys.stdout.write(str(the_num) + ' ')
-        result += the_num
-    sys.stdout.write(f"{the_num}\n")
+# Process input - separate into floats        
+def process_txt(all_lines):
+    all_coords = []
+    for line in all_lines:
+        vector = []
+        numbers = line.split()
+        for number in numbers:
+            n = float(number)
+            vector.append(n)
+        all_coords.append(vector)
+    return all_coords
+
+# Calculate the scalar sum
+def calc_vectors(all_coords):
+    result = 0
+    lines = len(all_coords) - 1
+    points = len(all_coords[0])
+    for i in range(lines):
+        for j in range(points):
+            the_num = all_coords[i][j] * all_coords[i+1][j]
+            result += the_num
+    return result 
+
+if __name__ == "__main__":
+       main()
